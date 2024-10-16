cd4b281888dfe8e2f7c0f52c5c8a08aaa3ce87e5
ArinzeJeffrey-droid <nnamenearinze@gmail.com>
2024-10-16 09:40:03 +0100

    feat: install agore dependencies.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Basics from './Basic'
+
+function App() {
+
+  return (
+    <>
+      <h1>Call test</h1>
+      <Basics />
+    </>
+  )
+}
+
+export default App
---
+++ src/Basic.tsx
@@ -0,0 +1,91 @@
+import { LocalUser, RemoteUser, useIsConnected, useJoin, useLocalMicrophoneTrack, usePublish, useRemoteUsers } from "agora-rtc-react";
+import { useState } from "react";
+
+export const Basics = () => {
+  const [calling, setCalling] = useState(false);
+  const isConnected = useIsConnected(); // Store the user's connection status
+  const [appId, setAppId] = useState("");
+  const [channel, setChannel] = useState("");
+  const [token, setToken] = useState("");
+
+  useJoin({appid: appId, channel: channel, token: token ? token : null}, calling);
+
+  const [micOn, setMic] = useState(true);
+  const { localMicrophoneTrack } = useLocalMicrophoneTrack(micOn);
+  usePublish([localMicrophoneTrack]);
+
+  const remoteUsers = useRemoteUsers();
+  console.log("remoteUsers", remoteUsers);
+  console.log("isConnected", isConnected);
+
+
+  return (
+    <>
+      <div className="room">
+        {isConnected ? (
+          <div className="user-list">
+            <div className="user">
+              <LocalUser
+                audioTrack={localMicrophoneTrack}
+                micOn={micOn}
+                cover="https://placehold.co/600x400"
+              >
+                <samp className="user-name">You</samp>
+              </LocalUser>
+            </div>
+            {remoteUsers.map((user) => (
+              <div className="user" key={user.uid}>
+                <RemoteUser cover="https://placehold.co/600x400" user={user}>
+                  <samp className="user-name">{user.uid}</samp>
+                </RemoteUser>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="join-room">
+            <input
+              onChange={e => setAppId(e.target.value)}
+              placeholder="<Your app ID>"
+              value={appId}
+            />
+            <input
+              onChange={e => setChannel(e.target.value)}
+              placeholder="<Your channel Name>"
+              value={channel}
+            />
+            <input
+              onChange={e => setToken(e.target.value)}
+              placeholder="<Your token>"
+              value={token}
+            />
+
+            <button
+              className={`join-channel ${!appId || !channel ? "disabled" : ""}`}
+              disabled={!appId || !channel}
+              onClick={() => setCalling(true)}
+            >
+              <span>Join Channel</span>
+            </button>
+          </div>
+        )}
+      </div>
+      {isConnected && (
+        <div className="control">
+          <div className="left-control">
+            <button className="btn" onClick={() => setMic(a => !a)}>
+              <i className={`i-microphone ${!micOn ? "off" : ""}`} />
+            </button>
+          </div>
+          <button
+            className={`btn btn-phone ${calling ? "btn-phone-active" : ""}`}
+            onClick={() => setCalling(a => !a)}
+          >
+            {calling ? <i className="i-phone-hangup" /> : <i className="i-mdi-phone" />}
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Basics;
